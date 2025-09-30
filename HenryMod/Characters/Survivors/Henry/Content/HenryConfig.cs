31fe560230e44f3d98fc4c33ce27d4aa237a1222
Rypling <djjohnnypalau@yahoo.com>
2025-09-30 13:36:11 -0400

    Initialize rewrite1

---
+++ HenryMod/Characters/Survivors/Henry/Content/HenryConfig.cs
@@ -0,0 +1,12 @@
+﻿namespace HenryMod.Survivors.Henry
+{
+    public static class HenryConfig
+    {
+        public static void Init()
+        {
+            string section = "Henry";
+
+            //add more here or else you're cringe
+        }
+    }
+}
